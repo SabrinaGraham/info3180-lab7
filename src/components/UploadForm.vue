96b150278320b028458bd8a44e572b961fa826f3
Sabrina Graham <techiepotato3004@gmail.com>
2022-04-10 21:58:03 -0400

    Uploadform component and Upload view

---
+++ src/components/UploadForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <form @submit.prevent="uploadPhoto" action="{{ url_for('upload') }}" method="POST" enctype="multipart/form-data">
+        <label>Description</label>
+        <input type="text" name="desc">
+        <br>
+        <label>Photo Upload</label>
+        <input type="file" name="photo">
+        <br>
+        <button type="submit" name="submit" class="btn btn-primary">Upload file</button>
+    </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      csrf_token: "",
+      response: null,
+    };
+  },
+    methods:{
+        uploadPhoto() {
+            
+            fetch("/api/upload", {
+                method: 'POST'
+            })
+            
+            .then(function (response) {
+                return response.json();
+            })
+            
+            .then(function (data) {
+                // display a success message
+                console.log(data);
+            })
+            
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
+
+    }
+    
+</script>
---
+++ src/views/UploadView.vue
@@ -0,0 +1,16 @@
+<script>
+import UploadForm from '@/components/UploadForm.vue'
+export default{
+    data(){},
+    components: {UploadForm}
+}
+</script>
+
+<template>
+    <div class= "upload">
+        <h2>Upload Form</h2>
+        <br>
+        <UploadForm/>
+    </div>
+
+</template>
